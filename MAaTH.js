873c4acfbfe6ada604e7e136e9807f7c121b9b04
ShielaMaeBacalando <chooksterchookie@gmail.com>
2025-11-17 21:31:31 +0800

    prod

--- MAaTH.js
+++ MAaTH.js
@@ -9,3 +9,11 @@ function quo (num1,num2){
     quo = num1 / num2;
     console.log(quo);
 }
+quo(2,3);
+
+function prod (num1,num2){
+    prod = num1 * num2;
+    console.log(prod);
+}
+
+prod(2,3);
