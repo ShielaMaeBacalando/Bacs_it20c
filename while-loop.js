43aba5d296738bbfdb756a26aed528587b8a0034
ShielaMaeBacalando <chooksterchookie@gmail.com>
2025-11-20 19:30:07 +0800

    Correct way of array in while-loop

--- while-loop.js
+++ while-loop.js
@@ -12,3 +12,13 @@ while (x <= 5) {
     console.log(x);
     x++;
 }
+arr = [4, 2, 3];
+output = "";
+index = 0;
+
+while (index <= arr.length - 1) {
+    output += arr[index] + " ";
+    index++;
+}
+
+console.log(output);
