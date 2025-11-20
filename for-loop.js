7477c763d23e1ac8d08fb14ef0e734bb8446ba66
ShielaMaeBacalando <chooksterchookie@gmail.com>
2025-11-20 19:24:01 +0800

    Added for-loop.js

---
+++ for-loop.js
@@ -0,0 +1,17 @@
+// for loop
+// output = "";
+
+// for (i = 10; i <= 5; i++) {
+// console.log
+// output += i + " ";
+
+// }
+// console.log(output);
+// Arrays
+output = "";
+arr = [1, 2, 3];
+console.log("Array length: " + arr.length);
+for (index = arr.length - 1; index >= 0; i--) {
+output += arr[index] + " ";
+}
+console.log(output);
