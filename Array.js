2457977b89cb7acc7143320b2f1c0e06bab950aa
ShielaMaeBacalando <chooksterchookie@gmail.com>
2025-11-24 08:26:55 +0800

    Added Array Update method for DSA/Array.js

--- Array.js
+++ Array.js
@@ -18,12 +18,50 @@ class Array{
        
         console.log(output);
     }
-}        
-    //
-    
-    arr1 = new Array([4, 2, 3]);
-    arr1.traverse();
+    insert(value){
+        //Correcting statement
+        if(index < 0 || index > this.length){
+            console.log("Invalid index");
+            return;
+        }
 
-    arr2 = new Array([3,2,1]);
-    arr2.traverse();
+        //Shifting conditions
+        for(let i = this.length; i > index; i--){
+            this.data[i] = this.data[i - 1];
+        }
+        this.data[index] = data;
+        this.length++;
+    }
+    search(element){
+        for(let i = 0; i < this.length; i++){
+            if(this.data[i] === element) 
+            console.log(element + " is found at index" + i);
+        return i;
+        }
+        
+        console.log(element + " is not found at any index");
+        return -1;
+    }  
+    
+    update(index, element){
+        if(index < 0 || index >= this.length){
+            console.log("Invalid index");
+            console.log();
+        }
+        this.data[index] = element;
+}   //1
+    // arr1 = new Array([4, 2, 3]);
+    // arr1.traverse();
+
+    // arr2 = new Array([3,2,1]);
+    // arr2.traverse();
+
+    //2
+//    arr1 = new Array([4, 2, 3]);
+//    arr1.traverse();
+//    arr1.insert(0,50);
+//    arr1.traverse();
+//    arr1.search(50);
+//    arr1.search(10);
+
 
